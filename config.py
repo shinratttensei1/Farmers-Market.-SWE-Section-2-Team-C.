c63b0d2785f5be5c65cd4abb6ca72eb624e0774a
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2024-11-21 02:01:28 +0500

    push last

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'biba')
 
     # MySQL Database Connection
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:bibaris@localhost/farmersmarket'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql7746150:hPpSM1pFIN@sql7.freesqldatabase.com/sql7746150'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
