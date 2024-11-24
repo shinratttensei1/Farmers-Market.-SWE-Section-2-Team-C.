aad2330e7ea49d89ccfd51af9f446169b908aed2
csoenjoyer <yessenzhan.aitbay@nu.edu.kz>
2024-11-24 21:53:23 +0500

    sds

--- bazaar/app/(tabs)/products.jsx
+++ bazaar/app/(tabs)/products.jsx
@@ -1,14 +1,20 @@
 import { View, Text } from 'react-native'
+import { SafeAreaView } from "react-native-safe-area-context"
 import React from 'react'
+import { images } from "../../constants"
 import "../global.css"
 
 const Products = () => {
   return (
-    <View
-      // className='w-10 h-10'
-    >
-      <Text>Products</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      {/* <View className="mt-1.5">
+          <Image
+            source={images.bazaar_logo}
+            className="w-9 h-10"
+            resizeMode="contain"
+          />
+      </View> */}
+    </SafeAreaView>
   )
 }
 
