bf9e26e2809efacb5952df91d6875214de7fa415
Bibarys Mukhambetiyar <bibarys.mukhambetiyar@nu.edu.kz>
2024-11-26 03:18:45 +0500

    fetched marketplace

--- bazaar/app/(tabs)/products.jsx
+++ bazaar/app/(tabs)/products.jsx
@@ -8,7 +8,7 @@ const Products = () => {
 
   // Fetch products from the API
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/marketplace/")
+    fetch("https://sersidw.pythonanywhere.com//marketplace/")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
